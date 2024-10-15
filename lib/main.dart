bee80448965452855845ee9892605df106483907
sara28g <sara.garah28@gmail.com>
2024-10-15 13:34:58 +0300

    adding LOG IN PAGE

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'sara',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey.shade200),
         useMaterial3: true,
       ),
@@ -25,7 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
@@ -38,64 +36,86 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text('log-in',style: TextStyle(color:Colors.white,fontSize: 30,)),
-
+        title: Text('log-in',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            )),
         backgroundColor: Colors.blueGrey.shade800,
-
-
       ),
       body: Center(
-      child: SquareText(),
+        child: SquareText(),
       ),
     );
-
   }
 }
-class SquareText extends StatelessWidget{
+
+class SquareText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width:350,
+      width: 350,
       height: 300,
-      decoration: BoxDecoration(color: Colors.blueGrey.shade200,
-    shape:
-    BoxShape.rectangle,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade200,
+        shape: BoxShape.rectangle,
         borderRadius: BorderRadius.circular(20),
-    ),
-    child: Center(
-      child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:[
-      Text("UserName or Email:",style: TextStyle(color:Colors.black,fontSize: 20,),textAlign: TextAlign.left , ),
-      SizedBox(width:300,
-          child:TextField(
-            decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'UserName/Email',),
-
-          )
       ),
-      Text("password:",style: TextStyle(color:Colors.black,fontSize: 20,),textAlign: TextAlign.left , ),
-      SizedBox(width:300,
-          child:TextField(
-            decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'password',),
-    )
-    ),
-],
-          ),
-          ),
-    );
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "UserName or Email:",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'UserName/Email',
+                  ),
+                )),
+            Text(
+              "password:",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'password',
+                  ),
+                )),
+            TextButton(
+              style: const ButtonStyle(
+                // foregroundColor: WidgetStateProperty.all<Color>(Colors.blue),
 
+              ),
+              onPressed: () { },
+              child: const Text('dont have an account sing up'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
-
-
-
